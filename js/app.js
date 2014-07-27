58bc28c489825dd4af0711738d6f09dd55743460
andraroston <andraroston@Andras-MacBook-Air.local>
2014-07-27 13:59:08 +0100

    New Game
    New game functionality, as well as random number generation.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
 
 $(document).ready(function(){
+
+	var humanInput = $("#userGuess").val();
+
+	var counter = 0;
+
+	var answerNumber = function() {
+		computerNumber = Math.floor(Math.random() * 101);
+			console.log(computerNumber);
+	}
+
+	answerNumber();
+
+	/*--- Start New Game ---*/
+	$(".new").click(function() {
+		$("ul").empty();
+		var counter = 0;
+		answerNumber();
+	})
+
+	/*--- During a Game ---*/
+
+
+
+	$("#count").replaceWith(counter++);
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
