4a5d3453c38691f52173f752a4e2c9135ca1d5f1
andraroston <andraroston@Andras-MacBook-Air.local>
2014-07-29 21:00:53 +0100

    Finished Product
    Will eventually restyle, but for now, all functionality is there!

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-	var guessNumber = $("#userGuess").val();
 
 	var answerNumber = function() {
 			computerNumber = Math.floor(Math.random() * 101);
@@ -10,56 +9,74 @@ $(document).ready(function(){
 
 	answerNumber();
 
+	var counter = 0;
+	var counting = function(){
+  		counter += 1;
+   		$('#count').replaceWith("<span id='count'>" + counter + "</span>");
+  	};
 
 	function gamePlay() { 
 
-	var guessNumber = $("#userGuess").val();
-		console.log(guessNumber);	
+		var guessNumber = $("#userGuess").val();
+			console.log(guessNumber);	
 
-	computerNumber;
+		computerNumber;
 
-	var guessChecker = (Math.abs(guessNumber - computerNumber));
-		console.log(guessChecker);
+		var guessChecker = (Math.abs(guessNumber - computerNumber));
+			console.log(guessChecker);
 
 
-	if(guessChecker === 0) {
-		$("#feedback").replaceWith("<h2 id='feedback'>" + "You got it right - press the New Game button to start again!" + "</h2>");
-	}
+		if(guessChecker === 0) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You got it right - press the New Game button to start again!" + "</h2>");
+		}
 
-	else if(guessChecker >= 30 && guessChecker <=49) {
-		$("#feedback").replaceWith("<h2 id=feedback>" + "You're cold!" + "</h2>");
-	}
+		else if(guessChecker >= 30 && guessChecker <=49) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're cold!" + "</h2>");
+		}
 
-	else if(guessChecker >= 20 && guessChecker <=29) {
-		$("#feedback").replaceWith("<h2 id='feedback'>" + "You're warm!" + "</h2>");
-	}
+		else if(guessChecker >= 20 && guessChecker <=29) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're warm!" + "</h2>");
+		}
 
-	else if(guessChecker >= 10 && guessChecker <=19) {
-		$("#feedback").replaceWith("<h2 id='feedback'>" + "You're hot!" + "</h2>");
-	}
+		else if(guessChecker >= 10 && guessChecker <=19) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're hot!" + "</h2>");
+		}
 
-	else if(guessChecker >= 1 && guessChecker <=9) {
-		$("#feedback").replaceWith("<h2 id='feedback'>" + "You're burning up!" + "</h2>");
-	}
+		else if(guessChecker >= 1 && guessChecker <=9) {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're burning up!" + "</h2>");
+		}
 
-	else {
-		$("#feedback").replaceWith("<h2 id='feedback'>" + "You're freezing!" + "</h2>");
-	}
+		else {
+			$("#feedback").replaceWith("<h2 id='feedback'>" + "You're freezing!" + "</h2>");
+		}
 
-};
+	};
+
 
 	$("#guessButton").on("click", function(){
 		
-		/* add validating of guessNumber */
+		event.preventDefault();
 
-			var guessNumber = $("#userGuess").val();
-			$("#guessList").append("<li>" + guessNumber + "</li>")
-			gamePlay();
+		var guessNumber = $("#userGuess").val();
+			if(guessNumber > 1 && guessNumber < 101) {
+				$("#guessList").append("<li>" + guessNumber + "</li>");
+				gamePlay();
+				counting();
+			} else {
+			alert("Not a valid number, try again!")
+			}
 			$("#userGuess").val(" ");
-		
 		
 	}); 
 
+	$(".new").on("click", function() {
+		$("#guessList").empty();
+		answerNumber();
+		var counter = 0;
+		$("#feedback").replaceWith("<h2 id='feedback'>" + "Make your Guess!" + "</h2>");
+		$('#count').replaceWith("<span id='count'>" + 0 + "</span>");
+
+	})
 
 	/*--- Display information modal box ---*/
 
